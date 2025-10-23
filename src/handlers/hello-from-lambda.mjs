8ac4c7b1eb3d171e54f267bbfab934aa8d3e2768
robz <rob@rob.com>
2025-10-23 13:41:31 -0700

    Add API Gateway in template.yaml, update function handler to return RESTful response

--- src/handlers/hello-from-lambda.mjs
+++ src/handlers/hello-from-lambda.mjs
@@ -1,12 +1,23 @@
 /**
  * A Lambda function that returns a static string
  */
-export const helloFromLambdaHandler = async () => {
-    // If you change this message, you will need to change hello-from-lambda.test.mjs
-    const message = 'Waazzzuuupp from Lambda bitches!';
+// export const helloFromLambdaHandler = async () => {
+//     // If you change this message, you will need to change hello-from-lambda.test.mjs
+//     const message = 'Simple Sam says waazzzuuupp from Lambda bitches!';
 
-    // All log statements are written to CloudWatch
-    console.info(`${message}`);
+//     // All log statements are written to CloudWatch
+//     console.info(`${message}`);
     
-    return message;
-}
+//     return message;
+// }
+
+export const helloFromLambdaHandler = async (event, context) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello world from first local lambda',
+      })
+    };
+
+    return response;
+  };
